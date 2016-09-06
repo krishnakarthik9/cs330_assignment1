6e1e5a7ed1f9765d87fe607db7c7777ff59b02df
gowtham-prudhvi <v.g.prudhvi38@gmail.com>
2016-09-06 22:40:59 +0530

    compiled code and added a new system call in exception.cc

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -157,7 +157,19 @@ ExceptionHandler(ExceptionType which)
        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
-    } else {
+    } 
+	
+    else if ((which == SyscallException) && (type == SYScall_GetReg)) {
+        int reg_num=(int)(machine->ReadRegister(4));
+	int curr_reg=(int)(machine->ReadRegister(reg_num)); 
+	machine->WriteRegister(2,curr_reg); 
+       // Advance program counters.
+       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    }
+
+	else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
     }
